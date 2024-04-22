b4a29fc25971613dab2717fd4e38cc8030076761
Nikhil-Gowda-T-P <76807414+Nikhil-Gowda-T-P@users.noreply.github.com>
2024-04-22 08:30:35 +0530

    Update Listing.jsx

--- client/src/newpages/Listing.jsx
+++ client/src/newpages/Listing.jsx
@@ -88,7 +88,7 @@ export default function Listing() {
               Link copied!
             </p>
           )}
-          <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+          <div className='flex flex-col max-w-4xl text-white mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
               {listing.name} - ${' '}
               {listing.offer
